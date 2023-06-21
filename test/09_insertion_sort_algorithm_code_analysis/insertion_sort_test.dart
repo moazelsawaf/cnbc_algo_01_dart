8924cd9d1b490ede2617f8d97c27be8f2fcd88b9
Moaz El-sawaf <moazelsawaf@hotmail.com>
2023-06-21 05:56:06 +0200

    chore: :pencil2: fix the name of single item test in insertion sort

--- test/09_insertion_sort_algorithm_code_analysis/insertion_sort_test.dart
+++ test/09_insertion_sort_algorithm_code_analysis/insertion_sort_test.dart
@@ -21,7 +21,7 @@ void main() {
       expect(x, [12, 12.2, 15, 45.52, 125, 145, 456.15]);
     });
 
-    test('with x single value of [5]', () async {
+    test('with single x value of [5]', () async {
       final x = [5];
 
       insertionSort(x);
