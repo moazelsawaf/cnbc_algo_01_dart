391b784f025fc73806ff2e4a89de576594420a18
Moaz El-sawaf <moazelsawaf@hotmail.com>
2023-06-22 05:19:43 +0200

    feat: :sparkles: add bonus dart specific implementation for segregate numbers algorithm

---
+++ lib/15_segregate_positive_and_negative_numbers_code/bonus_dart_specific_segregate_numbers.dart
@@ -0,0 +1,42 @@
+void segregateNumbers(List<double> array, int start, int end) {
+  if (end <= start) return;
+
+  final mid = (end + start) ~/ 2;
+
+  segregateNumbers(array, start, mid);
+  segregateNumbers(array, mid + 1, end);
+  merge(array, start, mid, end);
+}
+
+void merge(List<double> array, int start, int mid, int end) {
+  final leftArray = array.getRange(start, mid + 1).toList();
+  final rightArray = array.getRange(mid + 1, end + 1).toList();
+
+  int i = 0;
+  int j = 0;
+  int k = start;
+
+  while (i < leftArray.length && leftArray[i] < 0) {
+    array[k] = leftArray[i];
+    i++;
+    k++;
+  }
+
+  while (j < rightArray.length && rightArray[j] < 0) {
+    array[k] = rightArray[j];
+    j++;
+    k++;
+  }
+
+  while (i < leftArray.length) {
+    array[k] = leftArray[i];
+    i++;
+    k++;
+  }
+
+  while (j < rightArray.length) {
+    array[k] = rightArray[j];
+    j++;
+    k++;
+  }
+}
---
+++ test/15_segregate_positive_and_negative_numbers_code/bonus_dart_specific_segregate_numbers_test.dart
@@ -0,0 +1,40 @@
+import 'package:test/test.dart';
+
+import 'package:cloudnativebasecamp_algo_01_dart/15_segregate_positive_and_negative_numbers_code/bonus_dart_specific_segregate_numbers.dart';
+
+void main() {
+  group('(Bonus) (Dart Specific) Segregate Negative and Positive Numbers', () {
+    // NOTE: The same test data in the video
+    test('with array values of [6, -5, 12, 10, -9, -1]', () async {
+      final array = <double>[6, -5, 12, 10, -9, -1];
+
+      segregateNumbers(array, 0, array.length - 1);
+
+      expect(array, [-5, -9, -1, 6, 12, 10]);
+    });
+
+    test('with array values of [1, 2, 3] (All Positive)', () async {
+      final array = <double>[1, 2, 3];
+
+      segregateNumbers(array, 0, array.length - 1);
+
+      expect(array, [1, 2, 3]);
+    });
+
+    test('with array values of [-1, -2, -3] (All Negative)', () async {
+      final array = <double>[-1, -2, -3];
+
+      segregateNumbers(array, 0, array.length - 1);
+
+      expect(array, [-1, -2, -3]);
+    });
+
+    test('with array values of [-1, 0, 1, -2, 2] (Contains Zero)', () async {
+      final array = <double>[-1, 0, 1, -2, 2];
+
+      segregateNumbers(array, 0, array.length - 1);
+
+      expect(array, [-1, -2, 0, 1, 2]);
+    });
+  });
+}
