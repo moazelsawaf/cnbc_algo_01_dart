f7ea4cc430a96f64f880db336185756987f7d5f0
Moaz El-sawaf <moazelsawaf@hotmail.com>
2023-06-30 03:57:33 +0200

    refactor: :truck: `cloudnativebasecamp_algo_01_dart` -> `playground`

---
+++ bin/playground.dart
@@ -0,0 +1,12 @@
+void main(List<String> arguments) {
+  // Welcome to the Playground!
+  //
+  // This is a place where you can play with the algorithms in the repository.
+  // Feel free to add your own code and run it.
+  //
+  // For example:
+  //
+  // final circleRadius = 2.5;
+  // final circleArea = calculateCircleArea(circleRadius);
+  // print('The area of a circle with radius $circleRadius is $circleArea');
+}
---
+++ test/playground_test.dart
@@ -0,0 +1,15 @@
+void main() {
+  // Welcome to the Playground Test!
+  //
+  // Here you can write tests for the code in playground.dart file.
+  //
+  // For example:
+  //
+  // test('calculateCircleArea() returns correct value', () {
+  //   final circleRadius = 2.5;
+
+  //   final circleArea = calculateCircleArea(circleRadius);
+
+  //   expect(circleArea, 19.634954084936208);
+  // });
+}
